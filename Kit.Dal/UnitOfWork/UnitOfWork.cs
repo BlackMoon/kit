b5b691500a408070dc5ec15c46fa3466220e6c58
Роман Романов <romanovrv@tatintec.ru>
2016-02-20 16:02:49 +0300

    commit

--- Kit.Dal/UnitOfWork/UnitOfWork.cs
+++ Kit.Dal/UnitOfWork/UnitOfWork.cs
@@ -2,23 +2,52 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Linq;
+using Kit.Dal.Repository;
 using Kit.Kernel.UnitOfWork;
 
 namespace Kit.Dal.UnitOfWork
 {
-    public class UnitOfWork<T> : IUnitOfWork, IDisposable where T : DataContext
+    /// <summary>
+    /// Unit of Work паттерн
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class UnitOfWork<T> : IUnitOfWork, IDisposable where T : DataContext, new()
     {
-        private Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
+        private bool _disposed;
 
-        public T Context { get; private set; }
+        private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
+
+        public T Context { get; }
 
         public UnitOfWork(IDbConnection conn)
         {
-            //Context = context;
+            Context = new T();
         }
+
+
+        /*public IRepository<TEntity> Repository<TEntity>() where TEntity : class
+        {
+            IRepository<TEntity> repo;
+
+            Type type = typeof(TEntity);
+
+            if (_repositories.ContainsKey(type))
+                repo = (IRepository<TEntity>)_repositories[type];
+
+            else
+            {
+                Type repositoryType = typeof(Repository<>);
+                repo = (IRepository<TEntity>)Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), Context);
+
+                _repositories.Add(type, repo);
+            }
+
+            return repo;
+        }*/
 
         public void BeginTransaction()
         {
+            
             throw new System.NotImplementedException();
         }
 
@@ -32,9 +61,21 @@ namespace Kit.Dal.UnitOfWork
             throw new System.NotImplementedException();
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                    Context.Dispose();
+            }
+
+            _disposed = true;
+        }
+
         public void Dispose()
         {
-            Context.Dispose();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
