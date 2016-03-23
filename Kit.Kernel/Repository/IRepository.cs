6acd4843b64b0d23e2fbca8c7241a8f54815b4ee
Роман Романов <romanovrv@tatintec.ru>
2016-03-23 15:05:19 +0300

    conditional build

--- Kit.Kernel/Repository/IRepository.cs
+++ Kit.Kernel/Repository/IRepository.cs
@@ -3,6 +3,10 @@ using System.Linq.Expressions;
 
 namespace Kit.Kernel.Repository
 {
+    /// <summary>
+    /// Интерфейс репозиторий
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public interface IRepository<TEntity> where TEntity : class
     {
         TEntity Find(Expression<Func<TEntity, bool>> predicate);
