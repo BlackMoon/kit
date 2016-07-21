3bb0e6eb408589806abd3cf2094b6ca4d572d46f
Роман Романов <romanovrv@tatintec.ru>
2016-07-21 16:29:39 +0300

    AutoComplete

--- Software/netcore/src/Kit.Kernel/Configuration/AppSettings.cs
+++ Software/netcore/src/Kit.Kernel/Configuration/AppSettings.cs
@@ -2,6 +2,11 @@
 {
     public class AppSettings
     {
+        /// <summary>
+        /// autocomplete="on|off" для input type="password"
+        /// </summary>
+        public bool AutoComplete { get; set; }
+
         public string Theme { get; set; }
 
         public string Title { get; set; }
