df8385cc3384dd1789606ed4e2c06957686bb9dd
Роман Романов <romanovrv@tatintec.ru>
2016-12-15 18:43:14 +0300

    BaseSettings

---
+++ src/Kit.Core/Configuration/BaseSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Kit.Core.Configuration
+{
+    /// <summary>
+    ///  Настройки приложения
+    /// </summary>
+    public class BaseSettings
+    {
+
+        /// <summary>
+        /// Тема приложения
+        /// </summary>
+        public string Theme { get; set; }
+
+        /// <summary>
+        /// Заголовок приложения
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
