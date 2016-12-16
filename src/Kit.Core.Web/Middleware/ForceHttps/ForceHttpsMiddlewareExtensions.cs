927e12ccbfd4baeed5960444166e4a406717fe23
Роман Романов <romanovrv@tatintec.ru>
2016-12-16 11:35:52 +0300

    middlewares

---
+++ src/Kit.Core.Web/Middleware/ForceHttps/ForceHttpsMiddlewareExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace Kit.Core.Web.Middleware.ForceHttps
+{
+    public static class ForceHttpsMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseForceHttps(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<Middleware.ForceHttps.ForceHttpsMiddleware>();
+        }
+
+        public static IApplicationBuilder UseForceHttps(this IApplicationBuilder app, int securePort)
+        {
+            return app.UseMiddleware<Middleware.ForceHttps.ForceHttpsMiddleware>(securePort);
+        }
+
+        public static IApplicationBuilder UseForceHttps(this IApplicationBuilder app, ForceHttpsOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            return app.UseMiddleware<Middleware.ForceHttps.ForceHttpsMiddleware>(options);
+        }
+    }
+}
---
+++ src/Kit.Core.Web/Middleware/ForceHttps/ForceHttpsOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Kit.Core.Web.Middleware.ForceHttps
+{
+    /// <summary>
+    /// Настройки HTTPS REDIRECT
+    /// </summary>
+    public class ForceHttpsOptions
+    {
+        /// <summary>
+        /// HTTPS port
+        /// </summary>
+        public int Port { get; set; } = 443;
+
+        /// <summary>
+        /// Обязятельные url для проверки 
+        /// </summary>
+        public IList<string> Paths { get; set; } = new List<string>();
+    }
+}
