c01836d44a13fd32fedc74bdb75730d31fb17f02
Роман Романов <romanovrv@aquilon-st.ru>
2017-02-01 09:27:38 +0300

    remove domain

--- src/Kit.Dal.Oracle/Domain/TnsNames/Query/TnsNamesQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using Kit.Core.CQRS.Query;
-
-namespace Kit.Dal.Oracle.Domain.TnsNames.Query
-{
-    public class TnsNamesQuery : IQuery
-    {
-        public string ProviderInvariantName { get; set; }
-    }
-}
