47486bd2391ac0b4fd52e142d88595872a57bab9
romanovrv <romanovrv@aquilon-st.ru>
2017-04-21 15:30:06 +0300

    partial Interface IDbManager

---
+++ Kit.Dal/DbManager/IDbManager.Async.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kit.Dal.DbManager
+{
+    public partial interface IDbManager
+    {
+        Task OpenAsync(CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<int> ExecuteNonQueryAsync(CommandType commandType, string commandText);
+    }
+}
